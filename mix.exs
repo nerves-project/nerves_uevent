157e1238f3fc122f8cfba21d1457a59ad2fd70c1
Frank Hunleth <fhunleth@troodon-software.com>
2025-01-06 16:11:56 -0800

    Allow both property_table v0.2.0 and v0.3.0
    This undoes an automatic update from Dependabot. Some users might not be
    ready for property_table v0.3.0, so don't force it on them.

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule NervesUEvent.MixProject do
 
   defp deps do
     [
-      {:property_table, "~> 0.3.0"},
+      {:property_table, "~> 0.2.0 or ~> 0.3.0"},
       {:elixir_make, "~> 0.6", runtime: false},
       {:ex_doc, "~> 0.22", only: :docs, runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
